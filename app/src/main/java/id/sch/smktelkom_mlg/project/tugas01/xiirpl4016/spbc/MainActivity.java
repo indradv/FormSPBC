65704b84985047b6ac01d6676cc839749e18133f
Krisna Putra F <contact.krisnapf@gmail.com>
2016-10-15 21:51:21 +0700

    initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/tugas01/xiirpl4016/spbc/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.tugas01.xiirpl4016.spbc;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
